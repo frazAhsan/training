16af33fae74972497a59aeaae496f2290d1c75a4
Fraz Ahsan <fraz.ahsan@phaedrasolutions.com>
2016-04-25 18:27:49 +0500

    Image, excel and zip allowed

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,7 @@
 class Medium < ActiveRecord::Base
   belongs_to :folder
   has_attached_file :file
-  validates_attachment :file, :content_type => { :content_type => %w(text/plain application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+  validates_attachment :file, :content_type => { :content_type => %w(text/plain application/pdf image/jpeg image/gif image/png application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/zip) }
+  # has_many :rights
+  # has_many :permissions, through: :rights
 end
