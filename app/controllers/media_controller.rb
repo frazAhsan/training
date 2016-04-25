d99577eee3c6e8acb91b3be5bd45730fff85bb30
Fraz Ahsan <fraz.ahsan@phaedrasolutions.com>
2016-04-25 11:00:44 +0500

    File form changed

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -78,6 +78,6 @@ class MediaController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def medium_params
-      params.fetch(:medium, {})
+      params.require(:medium).permit(:file, :folder_id)
     end
 end
