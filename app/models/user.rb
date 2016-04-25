127e314dad32d0eebc6f92477f767d268fe9cad0
Fraz Ahsan <fraz.ahsan@phaedrasolutions.com>
2016-04-25 17:29:54 +0500

    User default image added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/       
+  # has_many :rights
+  # has_many :permissions, through: :rights
 end
