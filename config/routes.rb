c0bebee0cb7ef0c21a7a0e03c2a772f4caa4f7b6
Fraz Ahsan <fraz.ahsan@phaedrasolutions.com>
2016-04-25 10:35:33 +0500

    Exception added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users
   resources :media
   resources :folders
+
+  post "/agree" => "users#agreed"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
